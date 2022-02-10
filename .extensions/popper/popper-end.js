3cb4c9c199ae4ae1f62b15bdb1e6915896086f3b
Jason Hord <jason.hord@gmail.com>
2022-02-10 08:58:38 -0500

    Adds scoping for "globals" in popper.

--- .extensions/popper/popper-end.js
+++ .extensions/popper/popper-end.js
@@ -1,30 +1,32 @@
 'use strict';
 
-let e_needles = {
-  "ebay.com":   ebay,
-};
+{
+  let e_needles = {
+    "ebay.com":   ebay,
+  };
 
-function ebay() {
-  (function () {
-    let dialogs = document.querySelectorAll('div.wnd');
-    for(let i = 0, l = dialogs.length; i < l; i++) {
-      let dialog = dialogs[i];
-      dialog.style.visibility = 'hidden';
-      console.log('popper: ebay: hiding dialog', dialog);
-    }
-  })();
-}
-
-var host = window.location.host.split('.');
-while(host.length > 0) {
-  var domain = host.join('.');
-  var needle = e_needles[domain];
-
-  if(typeof needle !== 'undefined') {
-    console.log('popper end ('+ domain+ ')');
-    needle();
-    break;
+  function ebay() {
+    (function () {
+      let dialogs = document.querySelectorAll('div.wnd');
+      for(let i = 0, l = dialogs.length; i < l; i++) {
+        let dialog = dialogs[i];
+        dialog.style.visibility = 'hidden';
+        console.log('popper: ebay: hiding dialog', dialog);
+      }
+    })();
   }
 
-  host.shift();
+  var host = window.location.host.split('.');
+  while(host.length > 0) {
+    var domain = host.join('.');
+    var needle = e_needles[domain];
+
+    if(typeof needle !== 'undefined') {
+      console.log('popper end ('+ domain+ ')');
+      needle();
+      break;
+    }
+
+    host.shift();
+  }
 }
--- .extensions/popper/popper-idle.js
+++ .extensions/popper/popper-idle.js
@@ -1,18 +1,20 @@
 'use strict';
 
-let e_needles = {
-};
+{
+  let e_needles = {
+  };
 
-var host = window.location.host.split('.');
-while(host.length > 0) {
-  var domain = host.join('.');
-  var needle = e_needles[domain];
+  var host = window.location.host.split('.');
+  while(host.length > 0) {
+    var domain = host.join('.');
+    var needle = e_needles[domain];
 
-  if(typeof needle !== 'undefined') {
-    console.log('popper idle ('+ domain+ ')');
-    needle();
-    break;
+    if(typeof needle !== 'undefined') {
+      console.log('popper idle ('+ domain+ ')');
+      needle();
+      break;
+    }
+
+    host.shift();
   }
-
-  host.shift();
 }
